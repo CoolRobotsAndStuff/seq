41d60d82fb4f0b601517bdad3386faef887b5fc6
ale <aledeum.saf@gmail.com>
2025-07-05 14:25:09 -0300

    added cross compilation to windows to buildsystem

--- nob.c
+++ nob.c
@@ -7,6 +7,11 @@
 
 #define BUILD "build/"
 
+typedef enum {
+    WINDOWS_64,
+    LINUX
+} Platform;
+
 int main(int argc, char** argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
@@ -20,19 +25,38 @@ int main(int argc, char** argv) {
 
     bool run = false;
     bool debug = false;
+    Platform platform = LINUX;
     while (argc) {
         char* opt = shift(argv, argc);
         if (streq(opt, "run")) {
             run = true;
         } else if (streq(opt, "debug")) {
             debug = true;
+        } else if (streq(opt, "windows")) {
+            platform = WINDOWS_64;
         }
     }
 
     Cmd cmd = {0};
-    nob_cc(&cmd);
+    switch (platform) {
+        case LINUX:
+            nob_cmd_append(&cmd, "gcc");
+            break;
+        case WINDOWS_64:
+            nob_cmd_append(&cmd, "x86_64-w64-mingw32-gcc");
+            break;
+    }
     cmd_append(&cmd, temp_sprintf("%s.c", target));
-    nob_cc_output(&cmd, temp_sprintf("%s%s", BUILD, target));
+    
+    switch (platform) {
+        case LINUX:
+            nob_cc_output(&cmd, temp_sprintf("%s%s", BUILD, target));
+            break;
+        case WINDOWS_64:
+            nob_cc_output(&cmd, temp_sprintf("%s%s.exe", BUILD, target));
+            break;
+    }
+
     if (debug) {
         cmd_append(&cmd, "-ggdb");
     }
@@ -40,7 +64,18 @@ int main(int argc, char** argv) {
     
     if (run) {
         cmd.count = 0;
-        cmd_append(&cmd, temp_sprintf("%s%s", BUILD, target));
+        switch (platform) {
+        case LINUX:
+            cmd_append(&cmd, temp_sprintf("%s%s", BUILD, target));
+            break;
+        case WINDOWS_64:
+            cmd_append(&cmd, 
+                "wineconsole",
+                "cmd.exe", "/K",
+                temp_sprintf("%s%s.exe", BUILD, target)
+            );
+            break;
+        }
         if (!cmd_run_sync(cmd)) return 1;
     }
 
