49a79e5beb7f49d986f32c334f238631f308178f
ale <aledeum.saf@gmail.com>
2025-07-06 11:44:50 -0300

    added valgrind option to build system

--- nob.c
+++ nob.c
@@ -25,6 +25,8 @@ int main(int argc, char** argv) {
 
     bool run = false;
     bool debug = false;
+    bool valgrind = false;
+    bool debug_expand_macros = false;
     Platform platform = LINUX;
     while (argc) {
         char* opt = shift(argv, argc);
@@ -34,20 +36,43 @@ int main(int argc, char** argv) {
             debug = true;
         } else if (streq(opt, "windows")) {
             platform = WINDOWS_64;
+        } else if (streq(opt, "valgrind")) {
+            valgrind = true;
+        } else if (streq(opt, "expand_macros")) {
+            debug_expand_macros = true;
         }
     }
 
+
     Cmd cmd = {0};
+
+    if (debug_expand_macros) {
+        char* new_target = temp_sprintf("%s.preprocessed", target);
+        cmd_append(&cmd, 
+            "gcc", "-E", "-P",
+            temp_sprintf("%s.c", target),
+            "-o", temp_sprintf("%s%s.c", BUILD, new_target)
+        );
+        if (!cmd_run_sync(cmd)) return 1;
+        target = new_target;
+        cmd.count = 0;
+    }
+
     switch (platform) {
         case LINUX:
-            nob_cmd_append(&cmd, "gcc");
+            nob_cmd_append(&cmd, "gcc", "-O3");
             break;
         case WINDOWS_64:
             nob_cmd_append(&cmd, "x86_64-w64-mingw32-gcc");
             break;
     }
-    cmd_append(&cmd, temp_sprintf("%s.c", target));
-    
+
+    if (debug_expand_macros) {
+        cmd_append(&cmd, temp_sprintf("%s%s.c", BUILD, target));
+    } else {
+        cmd_append(&cmd, temp_sprintf("%s.c", target));
+    }
+
     switch (platform) {
         case LINUX:
             nob_cc_output(&cmd, temp_sprintf("%s%s", BUILD, target));
@@ -57,26 +82,42 @@ int main(int argc, char** argv) {
             break;
     }
 
-    if (debug) {
+    if (debug || valgrind) {
         cmd_append(&cmd, "-ggdb");
     }
     if (!cmd_run_sync(cmd)) return 1;
+
     
     if (run) {
         cmd.count = 0;
-        switch (platform) {
-        case LINUX:
-            cmd_append(&cmd, temp_sprintf("%s%s", BUILD, target));
-            break;
-        case WINDOWS_64:
+        if (valgrind) {
+            char* outfile = temp_sprintf("%s%s.callgrind.out", BUILD, target);
             cmd_append(&cmd, 
-                "wineconsole",
-                "cmd.exe", "/K",
-                temp_sprintf("%s%s.exe", BUILD, target)
+                "valgrind",
+                "--tool=callgrind",
+                temp_sprintf("--callgrind-out-file=%s", outfile),
+                temp_sprintf("%s%s", BUILD, target)
             );
-            break;
+            if (!cmd_run_sync(cmd)) return 1;
+
+            cmd.count = 0;
+            cmd_append(&cmd, "kcachegrind", outfile);
+            if (!cmd_run_sync(cmd)) return 1;
+        } else {
+            switch (platform) {
+            case LINUX:
+                cmd_append(&cmd, temp_sprintf("%s%s", BUILD, target));
+                break;
+            case WINDOWS_64:
+                cmd_append(&cmd, 
+                    "wineconsole",
+                    "cmd.exe", "/K",
+                    temp_sprintf("%s%s.exe", BUILD, target)
+                );
+                break;
+            }
+            if (!cmd_run_sync(cmd)) return 1;
         }
-        if (!cmd_run_sync(cmd)) return 1;
     }
 
     return 0;
